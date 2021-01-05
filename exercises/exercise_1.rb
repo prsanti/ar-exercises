7d3078d5da507cd0876d94388e83f424d86cc2a6
Paulo Santiago <p.santiago796@gmail.com>
2021-01-05 01:39:42 +0000

    create 3 stores in database

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,16 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+# t.column :name, :string
+# t.column :annual_revenue, :integer
+# t.column :mens_apparel, :boolean
+# t.column :womens_apparel, :boolean
+
+burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: false)
+
+richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+
+gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts Store.count
